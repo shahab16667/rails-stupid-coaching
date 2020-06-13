2b09c6ba1b31477611e3a952cbe996e190ed171f
Shahab Jahangiri <shahab.j3@gmail.com>
2020-06-13 13:01:00 +1000

    added old routes to be fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   get '/ask', to: 'questions#ask'
-  get '/answer', to: 'questions#answer'
+  get '/answer', to: 'answers#answer'
+  # resources :questions do
+  #   collection do
+  #     get 'ask'
+  #     get 'answer'
+  #   end
+  # end
 end
