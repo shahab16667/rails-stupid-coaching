e0bc901b1dd01850052ec036b26647d12126d14b
Shahab Jahangiri <shahab.j3@gmail.com>
2020-06-15 18:19:21 +1000

    code the controller with new action and used form action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,11 @@ class QuestionsController < ApplicationController
     end
 
     def answer
-        
+        @question = params[:question]
+        if @question.blank?
+            @answer = "i can't hear you"
+        elsif @question = 'hello'
+            @answer = 'Good Bye!'
+        end
     end
 end
