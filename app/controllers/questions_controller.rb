cf56de3cfcd72cd12e2e8e74f6df5e3a9e0dd397
Shahab Jahangiri <shahab.j3@gmail.com>
2020-06-15 18:43:26 +1000

    the controller and view working need css styling

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,10 +5,20 @@ class QuestionsController < ApplicationController
 
     def answer
         @question = params[:question]
-        if @question.blank?
-            @answer = "i can't hear you"
-        elsif @question = 'hello'
-            @answer = 'Good Bye!'
+        # if @question.blank?
+        #     @answer = "i can't hear you"
+        # elsif @question = 'i am going to work'
+        #     @answer = 'Great'
+        # elsif @question.end_with?
+        #     @answer = 'Silly question get ready and go to work'
+        # elsif @question.include? 'who are you'
+        #     @answer = 'i am who i am !'
+        # end
+        if @question.end_with?('?')
+            @answer = 'Silly Question'.upcase
+            3.times do 
+                @question =  'hello'
+            end
         end
     end
 end
